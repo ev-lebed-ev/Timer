950c42a0f5e5663be4873ba68a8c85563c88400f
Evgeny Lebedev <evgeny.lebedev.swn@gmail.com>
2022-10-15 11:40:41 +0300

    asd

--- .webpack/prod.config.js
+++ .webpack/prod.config.js
@@ -13,6 +13,7 @@ module.exports = {
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "../docs"),
+    filename: "[fullhash].js",
   },
   cache: {
     type: "filesystem"
